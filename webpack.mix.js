f42fff2ce890285ea6366be78d18f9444b12784f
rklugman <rwklugman@gmail.com>
2022-04-05 22:17:45 +0200

    Nova 4 support

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,16 @@
 let mix = require('laravel-mix')
+let path = require('path')
 
-mix.setPublicPath('dist')
-  .js('resources/js/nova-echo.js', 'js')
+mix.alias({
+    'laravel-nova': path.join(__dirname, '../laravel/nova/resources/js/mixins/packages.js'),
+})
+
+mix.js('resources/js/nova-echo.js', 'js').vue({ version: 3 })
+    .webpackConfig({
+        externals: {
+            vue: 'Vue',
+        },
+        output: {
+            uniqueName: 'vendor/nova-echo',
+        }
+    })
